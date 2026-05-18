1b644c0b01cd83f02d5aa2887f04090e806c3f76
Vinay Kumar <vinaykgupta04@gmail.com>
2026-05-18 13:08:10 +0530

    Reverse a String, Using strrev()

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php 
- // Reverse a String
+/* Reverse a String, Using strrev() */
+
+$num = "vinay";
+$count = 0;
+while(isset($num[$count])){
+$count++;
+}
+$reverse = [];
+for($i=$count-1;  $i>=0;  $i--){
+	$reverse[] = $num[$i];
+}
+echo "<pre>";
+print_r($reverse);
 
 
 
