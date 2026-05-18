b368cab1bd4bbb1b3e27d106af5bfd25442722bd
Vinay Kumar <vinaykgupta04@gmail.com>
2026-05-18 13:46:39 +0530

    Check palindrome

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-/* Reverse a String, Using strrev() */
+/* Reverse a String, without using strrev() */
 
 $num = "vinay";
 $count = 0;
@@ -10,9 +10,44 @@ $reverse = [];
 for($i=$count-1;  $i>=0;  $i--){
 	$reverse[] = $num[$i];
 }
-echo "<pre>";
-print_r($reverse);
+// echo "<pre>";
+// print_r($reverse);
 
 
+/* Palindrome Check using strrev() */ 
+function palindrome($num){
+	$rev_num = strrev($num);
+
+	if ($num == $rev_num){
+		return "String is palindrome";
+	}else {
+		return "String is not palindrome";
+	}
+}
+
+// echo palindrome("sun");
+
+/* Palindrome Check without using strrev() */ 
+function palindrome($num){
+	$count = 0;
+
+	while(isset($num[$count])){
+		$count++;
+	}
+
+	$rev_num = [];
+	for($i=$count-1; $i>=0; $i--){
+		$rev_num[] = $num[$i];
+	}
+	$rev_string = implode("", $rev_num);
+
+	if ($num == $rev_string){
+		return "String is palindrome";
+	}else {
+		return "String is not palindrome";
+	}
+}
+
+// echo palindrome("sus");
 
 ?>
